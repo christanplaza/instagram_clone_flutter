60ccb5b3cc091d0d2acbe229b984432a01f41ed0
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2021-12-03 02:01:50 +0800

    Added Google Sign-in

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(scopes: <String>[
+  'email',
+  'https://www.googleapis.com/auth/contacts.readonly',
+]);
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,46 +14,93 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isSignedIn = false;
+  GoogleSignInAccount? _currentUser;
 
-  Widget buildHomeScreen() {
-    return const Text("You are now Signed In");
+  @override
+  void initState() {
+    super.initState();
+
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+
+      if (_currentUser != null) {
+        _handleGetContact(_currentUser!);
+      }
+    });
+
+    _googleSignIn.signInSilently();
   }
 
-  Scaffold buildSignInScreen() {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: 
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text("Instagram Clone",
-                style: TextStyle(fontSize: 32.0, color: Colors.black)),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Center(
-                  child: Text("Sign in with Google"),
-                ),
+  Future<void> _handleGetContact(GoogleSignInAccount signInAccount) async {}
+
+  Future<void> loginUser() async {
+    await _googleSignIn.signIn();
+  }
+
+  Future<void> logoutUser() async {
+    _googleSignIn.disconnect();
+  }
+
+  Widget _buildBody() {
+    GoogleSignInAccount? user = _currentUser;
+
+    if (user != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text("Instagram Clone",
+              style: TextStyle(fontSize: 32.0, color: Colors.black)),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
+            child: Text("Hello, ${user.displayName}"),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
+            child: ElevatedButton(
+              onPressed: logoutUser,
+              child: const Center(
+                child: Text("Logout"),
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text("Instagram Clone",
+              style: TextStyle(fontSize: 32.0, color: Colors.black)),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
+            child: ElevatedButton(
+              onPressed: loginUser,
+              child: const Center(
+                child: Text("Sign in with Google"),
+              ),
+            ),
+          )
+        ],
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (isSignedIn) {
-      return buildHomeScreen();
-    } else {
-      return buildSignInScreen();
-    }
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.black, Colors.white]),
+        ),
+        child: _buildBody(),
+      ),
+    );
   }
 }
