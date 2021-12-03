95101360a1ad76447993f675d9dd130cbb0a7061
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2021-12-03 14:29:22 +0800

    Home screen Bottom Navbar

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:instagram_clone/pages/SearchPage.dart';
+import 'package:instagram_clone/pages/UploadPage.dart';
+import 'package:instagram_clone/pages/ProfilePage.dart';
+import 'package:instagram_clone/pages/TimelinePage.dart';
+import 'package:instagram_clone/pages/NotificationsPage.dart';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(scopes: <String>[
   'email',
@@ -15,10 +21,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   GoogleSignInAccount? _currentUser;
+  late PageController pageController;
+  int pageIndex = 0;
 
   @override
   void initState() {
     super.initState();
+
+    pageController = PageController();
 
     _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
       setState(() {
@@ -43,31 +53,77 @@ class _HomePageState extends State<HomePage> {
     _googleSignIn.disconnect();
   }
 
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  whenPageChanges(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  onTapChangePage(int pageIndex) {
+    pageController.animateToPage(pageIndex,
+        duration: const Duration(milliseconds: 400), curve: Curves.bounceInOut);
+  }
+
   Widget _buildBody() {
     GoogleSignInAccount? user = _currentUser;
 
     if (user != null) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text("Instagram Clone",
-              style: TextStyle(fontSize: 32.0, color: Colors.black)),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
-            child: Text("Hello, ${user.displayName}"),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
-            child: ElevatedButton(
-              onPressed: logoutUser,
-              child: const Center(
-                child: Text("Logout"),
-              ),
-            ),
-          ),
-        ],
+      return Scaffold(
+        body: PageView(
+          children: const [
+            TimelinePage(),
+            SearchPage(),
+            UploadPage(),
+            NotificationsPage(),
+            ProfilePage(),
+          ],
+          controller: pageController,
+          onPageChanged: whenPageChanges,
+          physics: const NeverScrollableScrollPhysics(),
+        ),
+        bottomNavigationBar: CupertinoTabBar(
+          currentIndex: pageIndex,
+          onTap: onTapChangePage,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home)),
+            BottomNavigationBarItem(icon: Icon(Icons.search)),
+            BottomNavigationBarItem(
+                icon: Icon(
+              Icons.photo_camera,
+              size: 37.0,
+            )),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite)),
+            BottomNavigationBarItem(icon: Icon(Icons.person)),
+          ],
+        ),
       );
+      // return Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: [
+      //     const Text("Instagram Clone",
+      //         style: TextStyle(fontSize: 32.0, color: Colors.black)),
+      //     Padding(
+      //       padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
+      //       child: Text("Hello, ${user.displayName}"),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
+      //       child: ElevatedButton(
+      //         onPressed: logoutUser,
+      //         child: const Center(
+      //           child: Text("Logout"),
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // );
     } else {
       return Column(
         mainAxisAlignment: MainAxisAlignment.center,
---
+++ lib/pages/TimelinePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TimelinePage extends StatefulWidget {
+  const TimelinePage({Key? key}) : super(key: key);
+
+  @override
+  _TimelinePageState createState() => _TimelinePageState();
+}
+
+class _TimelinePageState extends State<TimelinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
